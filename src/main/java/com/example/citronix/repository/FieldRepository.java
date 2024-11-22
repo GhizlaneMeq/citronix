4752c5a4b98d2e6d63db09f293975ac5ce6ea59a
GhizlaneMeq <meqdarghizlane@gmail.com>
2024-11-22 11:39:29 +0100

    creation des fiels avec validation

--- src/main/java/com/example/citronix/repository/FieldRepository.java
+++ src/main/java/com/example/citronix/repository/FieldRepository.java
@@ -1,8 +1,11 @@
 package com.example.citronix.repository;
 
+import com.example.citronix.entity.Farm;
 import com.example.citronix.entity.Field;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FieldRepository extends JpaRepository<Field, Long> {
+import java.util.List;
 
+public interface FieldRepository extends JpaRepository<Field, Long> {
+    List<Field> findAllByFarm(Farm farm);
 }
--- src/main/java/com/example/citronix/service/implementation/FieldServiceImpl.java
+++ src/main/java/com/example/citronix/service/implementation/FieldServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.citronix.service.implementation;
 import com.example.citronix.dto.field.FieldCreateDTO;
 import com.example.citronix.entity.Farm;
 import com.example.citronix.entity.Field;
+import com.example.citronix.helpers.FieldValidationHelper;
 import com.example.citronix.mapper.FieldMapper;
 import com.example.citronix.repository.FieldRepository;
 import com.example.citronix.service.FarmService;
@@ -16,10 +17,19 @@ public class FieldServiceImpl implements FieldService {
     private final FieldRepository fieldRepository;
     private final FieldMapper fieldMapper;
     private final FarmService farmService;
+    private  final FieldValidationHelper fieldValidationHelper;
 
     @Override
     public Field save(FieldCreateDTO fieldCreateDTO) {
-        Farm farm = farmService.findById
-        return null;
+        Farm farm = farmService.findById(fieldCreateDTO.getFarmId());
+
+        fieldValidationHelper.validateFieldArea(farm.getId(), fieldCreateDTO.getArea());
+        fieldValidationHelper.validateFieldCount(farm.getId());
+
+        Field field = fieldMapper.toField(fieldCreateDTO);
+
+        field.setFarm(farm);
+
+        return fieldRepository.save(field);
     }
 }
